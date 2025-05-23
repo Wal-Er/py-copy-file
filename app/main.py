a1fd25283f84b76661ba5dca766c8dcf678c27fc
Walery Demidko <walerydz@gmail.com>
2025-05-23 13:22:06 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-# write your code here
+def copy_file(command: str) -> None:
+    if command:
+        command_ = command.split()
+        if len(command_) == 3 and command_[0] == "cp":
+            source_file = command_[1]
+            target_file = command_[2]
+            if source_file != target_file:
+                try:
+                    with (open(source_file, "r") as file_in,
+                          open(target_file, "w") as file_out):
+                        for line in file_in:
+                            file_out.write(line)
+                except FileNotFoundError:
+                    pass
